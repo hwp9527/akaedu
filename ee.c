2a3da8f60c2db7b80b8ef48c03ef573d9eb58a9e
WeiPing He <hwp195@gmail.com>
2012-01-14 09:31:00 +0800

    my second commit

---
+++ ee.c
@@ -0,0 +1,26 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  ee.c
+ *
+ *    Description:  asdfjsadlk;fj
+ *
+ *        Version:  1.0
+ *        Created:  01/14/2012 09:28:19 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  He Wei Ping (hwp), hwp195@gmail.com
+ *        Company:  AKAEDU
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    int i;
+	scanf("%d",&i);
+	return 0;
+}
