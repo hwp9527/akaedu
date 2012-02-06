b62a17b4af511ad22c8a39e3f65ff8475534aaf5
HeWeiPing <hwp195@gmail.com>
2012-02-06 15:51:51 +0800

    add qq.a

---
+++ qq.c
@@ -0,0 +1,19 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  qq.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  02/06/2012 03:50:55 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Mr. He Wei Ping (hwp), hwp195@gmail.com
+ *        Company:  
+ *
+ * =====================================================================================
+ */
+
+qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq
